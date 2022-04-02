06cc14506d5f6403bc9ff82afdaeb3e94ad3f221
Misty Duhart <mystie.smith@gmail.com>
2022-04-02 15:42:49 -0400

    js file

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+// Current Date and Time in Header
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 
+
+// Colored Time Blocks for Past, Present and Future
+function colorTimeBlock() {
+    var currentTimeBlock = moment().hour();
+    $(".time-block").each(function () {
+        
+    })
+}
