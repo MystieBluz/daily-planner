2d2efcea468fe79468913d96940ec7682db34870
Misty Duhart <mystie.smith@gmail.com>
2022-04-02 17:30:08 -0400

    html and js file

--- javascript.js
+++ javascript.js
@@ -37,5 +37,19 @@ $(document).ready(function (){
         })
     }
 
-    
+    // Local Storage
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#1 .description").val(localStorage.getItem("1"));
+    $("#2 .description").val(localStorage.getItem("2"));
+    $("#3 .description").val(localStorage.getItem("3"));
+    $("#4 .description").val(localStorage.getItem("4"));
+    $("#5 .description").val(localStorage.getItem("5"));
+
+
+
+    currentTime();
+
 })
