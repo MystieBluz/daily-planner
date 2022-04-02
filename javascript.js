ccebd3f7f3f48287c53cfb8afbe3d8534b0f61cf
Misty Duhart <mystie.smith@gmail.com>
2022-04-02 17:22:11 -0400

    js file

--- javascript.js
+++ javascript.js
@@ -2,11 +2,40 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 
+$(document).ready(function (){
+    // Save Button for Input in Text Area
+    $(".saveBtn").on("click", function (){
+        var input = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-// Colored Time Blocks for Past, Present and Future
-function colorTimeBlock() {
-    var currentTimeBlock = moment().hour();
-    $(".time-block").each(function () {
-        
+        // Save Input in Local Storage
+        localStorage.setItem(time, input);
     })
-}
+
+    // Timeblocks for specific time are color coded to indicate Past, Present or Future
+    function currentTime(){
+        var activeTime = moment().hour();
+
+        $(".time-block").each(function (){
+            var colorTimeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (colorTimeBlock < activeTime) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            else if (colorTimeBlock === activeTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    
+})
